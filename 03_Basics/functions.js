a8f4c3a8d1371a950ba621d23b47b47f7a959f4a
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-09-06 17:16:48 +0000

    done

---
+++ 03_Basics/functions.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    return a+b;
+
+}
+
+const result=add(10,10);
+
+console.log(result);
+
+// function with object and array in javascript
+
+function loggedin(username)
+{
+    return `${username} just logged in`
+}
+let a=loggedin("sneha")
+console.log(a)
