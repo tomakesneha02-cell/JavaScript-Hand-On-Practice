160ba14d5d7248abd8b68799d60bbad1ea4605d3
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-09-07 05:03:48 +0000

    config Done

--- 03_Basics/functions.js
+++ 03_Basics/functions.js
@@ -1,17 +1,32 @@
-function add(a,b){
-    return a+b;
+// function add(a,b){
+//     return a+b;
 
-}
+// }
 
-const result=add(10,10);
+// const result=add(10,10);
 
-console.log(result);
+// console.log(result);
 
-// function with object and array in javascript
+// // function with object and array in javascript
 
-function loggedin(username)
+// function loggedin(username)
+// {
+//     return `${username} just logged in`
+// }
+// let a=loggedin("sneha")
+// console.log(a)
+
+// spread Operator:  used to  expand the element of array or properties of object 
+
+let spread1 = [1, 2, 3]
+let spread2 = [...spread1]
+console.log(spread1)
+console.log(spread2)
+
+
+function test(val1,val2,...add)
 {
-    return `${username} just logged in`
+    return add
 }
-let a=loggedin("sneha")
-console.log(a)
+
+console.log(test(10,20,30))
---
+++ 03_Basics/scope.js
@@ -0,0 +1,11 @@
+var a = 10;
+function scope1() {
+    let b = 10
+    console.log("Sneha is here");
+    console.log("b's value ",b)
+
+}
+
+scope1()
+console.log("a's value", a)
+// console.log(b)
