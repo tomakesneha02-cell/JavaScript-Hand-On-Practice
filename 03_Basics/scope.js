5cb8fb77773d8d7e62d88e98b12cfe74981f60e0
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-09-07 07:00:15 +0000

    done

--- 03_Basics/scope.js
+++ 03_Basics/scope.js
@@ -1,11 +1,30 @@
-var a = 10;
-function scope1() {
-    let b = 10
-    console.log("Sneha is here");
-    console.log("b's value ",b)
+// var a = 10;
+// function scope1() {
+//     let b = 10
+//     console.log("Sneha is here");
+//     console.log("b's value ",b)
 
+// }
+
+// scope1()
+// console.log("a's value", a)
+// // console.log(b)
+// console.log("---------------------------");
+
+
+// const addTwo=function(num){
+//     return num+2;
+
+// }
+ 
+// addTwo(2)
+
+//Arrow Function 
+ 
+let result = (a,b) =>{
+     console.log(a+b,"This is arrow function");
+     
 }
 
-scope1()
-console.log("a's value", a)
-// console.log(b)
+result(1,1)
+console.log();
