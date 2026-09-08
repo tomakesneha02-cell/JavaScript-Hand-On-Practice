1a5fc6d9c5669adcac7f1a82d5600d542a120314
tomakesneha02-cell <tomakesneha02@gmail.com>
2026-09-08 18:37:57 +0000

    done

---
+++ ArrayIterationMethods/map.js
@@ -0,0 +1,59 @@
+// const map = new Map();
+
+// map.set('In', "India")
+// map.set('Fr', "Frace")
+// map.set('USA', "United state america")
+// // console.log(map);
+
+
+// // //for loop on map 
+// //  for (const key of map)
+// //  {
+// //     console.log(key);
+    
+// //  }
+
+
+//  for (const [key,value] of map)
+//  {
+//     console.log(key,":--",value);
+    
+//  }
+
+ //filter and reduce in javascript 
+
+
+ //Array Ietration Method:-- 
+
+ //1 : for each 
+
+//  const arr=["sneha","tomake"]
+
+//  arr.forEach((item)=>{
+//    console.log(item);
+   
+//  })
+// console.log(arr);
+
+//for each using function 
+
+let num=[1,2,3,4,5]
+
+num.forEach(function(item){
+   console.log(item);
+   
+})
+
+
+
+console.log("------------------------");
+
+num.forEach((index,value) =>{
+   console.log(value,index);
+   
+})
+
+
+//filter method :-create a new array that containing  new elemnt that statisfied the condition
+
+
